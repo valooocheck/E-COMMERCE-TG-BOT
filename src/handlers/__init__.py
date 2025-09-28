401a21d25a2b90108d42ea088a596d41817e8d77
user <valentinsysuev@mail.ru>
2025-09-28 20:31:44 +0300

    init handlers

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,5 +1,7 @@
 from handlers.v1.admin import admin
 from handlers.v1.admin.categories import add, delete, update
+from handlers.v1.admin.products import add as add_product
+from handlers.v1.admin.products import delete as delete_product
 from handlers.v1.main_menu import router
 
-routers = (router, admin.router, update.router, add.router, delete.router)
+routers = (router, admin.router, update.router, add.router, delete.router, add_product.router, delete_product.router)
